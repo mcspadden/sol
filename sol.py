8e9d262923c10ee29283af1a3b254094b638d94d
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-08-09 21:25:40 -0700

    index update

--- sol.py
+++ sol.py
@@ -6,4 +6,4 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main.html")  # main html page for sol
+        return render_template("index.html")  # index html page for sol
